11e7ef373f8e3044537073f01053823d24a01703
shanmukh49 <shanmukha49reddy@gmail.com>
2014-02-12 10:58:57 +0530

    first commit

---
+++ app/controllers/aboutus_controller.rb
@@ -0,0 +1,14 @@
+class AboutusController < ApplicationController
+ def about
+ end
+  def overview
+  end
+  def vision_mission_values
+  end
+   def testing
+  end
+  def slider
+  end
+  def video
+  end
+end
